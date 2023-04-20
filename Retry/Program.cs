6f633e7eed6d5786a7d5911a7cede067fbe2bd09
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-04-20 17:52:33 -0600

    Add Resiliencia

---
+++ Retry/Program.cs
@@ -0,0 +1,36 @@
+﻿using Polly;
+using System;
+
+public class Program
+{
+    public static async void Main(string[] args)
+    {
+        try
+        {
+            Policy.Handle<Exception>().Retry(3, (exception, rety) =>
+            {
+                Console.WriteLine($"Se jecuta {rety}");
+            }).Execute(() =>
+            {
+                throw new Exception();
+            });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Fallaron los reintentos");
+        }
+
+        await Policy.Handle<Exception>().RetryAsync(3, (exception, retry) =>
+        {
+            Console.WriteLine($"Se ejecuta la solicitud {retry}");
+        }).ExecuteAsync(async () =>
+        {
+            var client = new HttpClient();
+            var resp = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+
+            var content = await resp.Content.ReadAsStringAsync();
+
+            Console.WriteLine(content);
+        });
+    }
+}
---
+++ TimeOut/Program.cs
@@ -0,0 +1,20 @@
+﻿using Polly;
+using Polly.Timeout;
+
+try
+{
+    await Policy.TimeoutAsync(TimeSpan.FromSeconds(10), TimeoutStrategy.Pessimistic)
+        .ExecuteAsync(async () =>
+        {
+            var client = new HttpClient();
+            var resp = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+
+            var content = await resp.Content.ReadAsStringAsync();
+
+            Console.WriteLine(content);
+        });
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
